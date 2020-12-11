7b2a4db0a62911557c897df07581e45e219b556b
Justin-Lyy <justinlyjustin@gmail.com>
2020-12-10 21:20:51 -0500

    fixed a bug  where summoners with long names would cause the champion icon to narrow

--- src/components/matchComponents/Team.js
+++ src/components/matchComponents/Team.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import champions from '../../data/champions.js'
 
+function shorten(string) {
+    if (string.length > 13) {
+        return string.slice(0, 10) + "..."
+    }
+
+    return string
+}
+
 function Team (props) {
     return (
         <div className='team'>
@@ -8,31 +16,31 @@ function Team (props) {
                 <div className='champion-small'>
                     <img className='text' src={`http://ddragon.leagueoflegends.com/cdn/10.25.1/img/champion/${champions[props.team.top.champ]}.png`}></img>
                 </div>
-                <p>{props.team.top.name}</p>
+                <p textOverflow="ellipsis">{shorten(props.team.top.name)}</p>
             </div>
             <div className='flex-display'>
                 <div className='champion-small'>
                     <img src={`http://ddragon.leagueoflegends.com/cdn/10.25.1/img/champion/${champions[props.team.jungle.champ]}.png`}></img>
                 </div>
-                <p>{props.team.jungle.name}</p>
+                <p textOverflow="ellipsis">{shorten(props.team.jungle.name)}</p>
             </div>
             <div className='flex-display'>
                 <div className='champion-small'> 
                     <img src={`http://ddragon.leagueoflegends.com/cdn/10.25.1/img/champion/${champions[props.team.mid.champ]}.png`}></img>
                 </div>
-                <p>{props.team.mid.name}</p>
+                <p textOverflow="ellipsis">{shorten(props.team.mid.name)}</p>
             </div>
             <div className='flex-display'>
                 <div className='champion-small'>
                     <img src={`http://ddragon.leagueoflegends.com/cdn/10.25.1/img/champion/${champions[props.team.adc.champ]}.png`}></img>
                 </div>
-                <p>{props.team.adc.name}</p>
+                <p textOverflow="ellipsis">{shorten(props.team.adc.name)}</p>
             </div>
             <div className='flex-display'>
                 <div className='champion-small'>
                     <img src={`http://ddragon.leagueoflegends.com/cdn/10.25.1/img/champion/${champions[props.team.support.champ]}.png`}></img>
                 </div>
-                <p>{props.team.support.name}</p>
+                <p textOverflow="ellipsis">{shorten(props.team.support.name)}</p>
             </div>
         </div>
     )
